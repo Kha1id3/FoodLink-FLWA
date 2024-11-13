966ca50a4968af2158fc9b87b2a7339488e82604
KhalidMuzaffar269553 <khalodi303@gmail.com>
2024-11-13 21:06:41 +0100

    Update on package.json

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,7 @@ let favoriteRouter = require("./routes/favorites.js");
 let userRouter = require("./routes/user.js");
 let sessionRouter = require("./routes/sessions.js");
 
-const cors = require('cors');
-const app = express();
+var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -26,23 +25,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// Updated CORS configuration to allow credentials
-app.use(cors({
-  origin: 'http://localhost:5174',  // Replace with frontend origin
-  credentials: true  // Allow credentials (session cookies)
-}));
-
-// Updated session configuration
 app.use(
   session({
-    secret: "food_link",
+    secret: "save_a_plate",
     resave: false,
-    saveUninitialized: false,  // Do not save empty sessions
-    cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',  // Use secure cookies in production
-      maxAge: 24 * 60 * 60 * 1000  // 1 day cookie expiration
-    }
+    saveUninitialized: true
   })
 );
 
@@ -67,9 +54,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
   res.json("error");
 });
--- frontend/src/components/navBar/navComponents/aboutUs/AboutUs.js
+++ frontend/src/components/navBar/navComponents/aboutUs/AboutUs.js
@@ -6,24 +6,24 @@ export default function About() {
     {
       name: "Khalid Muzaffar",
       github: "https://github.com/your-github",
-      img: "path_to_khalid_image.jpg", // Update with actual image paths
+      img: "https://avatars.githubusercontent.com/u/149905898?v=4", // Update with actual image paths
     },
     {
       name: "Ziad Oun",
       github: "https://github.com/your-github",
-      img: "path_to_ziad_image.jpg",
+      img: "https://media.licdn.com/dms/image/v2/D4D03AQE4mRvR-1iZYQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1720092128049?e=1736985600&v=beta&t=yrJzCHIMcAmflijyztiboScOtuTp1Q-lGeZx0kKB3UU",
     },
     {
       name: "Mert Kodzhaaslan",
       github: "https://github.com/your-github",
-      img: "path_to_mert_image.jpg",
+      img: "./Morat logo.jpg",
     },
   ];
 
   return (
     <div id="about-container">
       <div id="about-sub-container">
-        <h1 id="meet-our-team">Meet Our Team</h1>
+        <h1 id="meet-our-team">Team Members</h1>
         <div id="about-us-container">
           {teamMembers.map((member, index) => (
             <div key={index} className="team-member">
