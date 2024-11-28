daacec3c96743cd859128210a2a548ce7f69a68b
Kha1id3 <khalodi303@gmail.com>
2024-11-28 17:02:12 +0100

    test

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
 const pgp = require("pg-promise")({});
 const db = pgp(
-  process.env.DATABASE_URL || "postgres://postgres:12345678@localhost:5432/FoodLink2"
+  process.env.DATABASE_URL || "postgres://postgres:12345678@localhost:5432/foodlink2"
 );
 
 module.exports = { db };
